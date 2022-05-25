c35dbf05d0e33951339fd3136ffe438c4b4594b1
Arpit Mathur <44263336+AMnitAMn@users.noreply.github.com>
2022-05-25 14:21:41 +0530

    Delete ShortURL directory

--- ShortURL/src/main/java/com/capg/shorturl/service/LinkService.java
+++
@@ -1,8 +0,0 @@
-package com.capg.shorturl.service;
-
-public interface LinkService {
-	
-	public String getShortURL(String localURL, String longURL);
-	
-	public String getLongURLfromID(String uniqueID) throws Exception;
-}
